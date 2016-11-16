903b771850d142087d8a56e9a0c6415ff63872aa
Jan <jan.evseev@gmail.com>
2016-11-16 16:32:09 +0300

    add solution

--- src/check.js
+++ src/check.js
@@ -2,4 +2,47 @@
 
 exports.init = function () {
 
+    function isFunction(testArg) {
+        return typeof testArg === 'function';
+    }
+
+    Object.prototype.checkHasKeys = Array.prototype.checkHasKeys = function (testKeys) {
+        if (isFunction(this)) {
+            return;
+        }
+        let objectKeys = Object.keys(this);
+        return testKeys.every(testKey => {
+            return objectKeys.includes(testKey);
+        });
+    };
+
+    Object.prototype.checkHasValueType =
+    Array.prototype.checkHasValueType = function (value, TypeConstructor) {
+        if (isFunction(this)) {
+            return;
+        }
+        return typeof this[value] === TypeConstructor.name.toLowerCase();
+    };
+
+    Object.prototype.checkContainsValues =
+    Array.prototype.checkContainsValues = function (testValues) {
+        if (isFunction(this)) {
+            return;
+        }
+        return testValues.every(value => {
+            return Object.values(this).includes(value);
+        });
+    };
+
+    Function.prototype.checkHasParamsCount = function (testParams) {
+        return this.length === testParams;
+    };
+
+    Array.prototype.checkHasLength = String.prototype.checkHasLength = function (testLength) {
+        return this.length === testLength;
+    };
+
+    String.prototype.checkHasWordsCount = function (wordCount) {
+        return this.split(' ').length === wordCount;
+    };
 };
